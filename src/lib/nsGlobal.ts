510c412c5ed0c4032202ca0f58b43712259315f3
DavidLoftus <43099493+DavidLoftus@users.noreply.github.com>
2022-06-17 11:34:37 +0100

    fix nsglobal

--- src/lib/nsGlobal.ts
+++ src/lib/nsGlobal.ts
@@ -1,24 +1,7 @@
 import { NS } from '@ns'
 
-export const ns: NS = new Proxy({} as NS, {
-    /* @ts-ignore */
-    get: (target, p, receiver) => globalThis.ns[p],
-});
-
-// interface NSContainer {
-//     ns: [hostname: string]: [args: string]: NS
-// }
-
-// function getPid(ns: NS): string {
-//     const process = ns.ps().find(proc => proc.filename == ns.getScriptName() && proc.args == ns.args);
-//     if (process instanceof undefined) {
-//         ns.tprint(`Failed to find process id with args ${JSON.stringify(ns.args)}`);
-//     }
-// }
+export let ns: NS
 
 export function init(_ns: NS): void {
-    const pid = _ns.ps()
-
-    /* @ts-ignore */
-    globalThis.ns = _ns;
+    ns = _ns;
 }
